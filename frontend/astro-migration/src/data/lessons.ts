62c407581a2932ab420ae2921be185dcfd0fbc0f
AntoineSPR <antoine.simper@gmail.com>
2026-01-19 02:44:13 +0100

    Create page : Cours de musique

---
+++ frontend/astro-migration/src/data/lessons.ts
@@ -0,0 +1,62 @@
+export interface Lesson {
+  title: string;
+  slug: string;
+  image: string;
+}
+
+export const lessons: Lesson[] = [
+  {
+    title: 'Guitare',
+    slug: 'guitare',
+    image:
+      'https://images.unsplash.com/photo-1510915361894-db8b60106cb1?w=800&q=80',
+  },
+  {
+    title: 'Batterie',
+    slug: 'batterie',
+    image:
+      'https://images.unsplash.com/photo-1519892300165-cb5542fb4747?w=800&q=80',
+  },
+  {
+    title: 'Basse',
+    slug: 'basse',
+    image:
+      'https://images.unsplash.com/photo-1445985543470-4102eb966c00?w=800&q=80',
+  },
+  {
+    title: 'Chant',
+    slug: 'chant',
+    image:
+      'https://images.unsplash.com/photo-1516280440614-6697288d5d38?w=800&q=80',
+  },
+  {
+    title: 'Clavier',
+    slug: 'clavier',
+    image:
+      'https://images.unsplash.com/photo-1520523839897-bd0b52f945a0?w=800&q=80',
+  },
+  {
+    title: 'Jeu en groupe',
+    slug: 'jeu-en-groupe',
+    image:
+      'https://images.unsplash.com/photo-1514320291840-2e0a9bf2a9ae?w=800&q=80',
+  },
+  {
+    title: 'Danse Hip Hop',
+    slug: 'danse-hip-hop',
+    image:
+      'https://images.unsplash.com/photo-1504609773096-104ff2c73ba4?w=800&q=80',
+  },
+  {
+    title: 'Éveil Musical',
+    slug: 'eveil-musical',
+    image:
+      'https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?w=800&q=80',
+  },
+  {
+    title: 'Petits Rockers',
+    slug: 'petits-rockers',
+    image:
+      'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=800&q=80',
+  },
+];
