677cd32727ed6f880733f02c2a9afdef15deea5b
AntoineSPR <antoine.simper@gmail.com>
2026-01-26 15:29:50 +0100

    Refactor paths

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function resolvePath(path: string) {
+  if (!path) return path;
+  if (path.startsWith('http')) return path;
+  const base = import.meta.env.BASE_URL;
+  const cleanPath = path.startsWith('/') ? path.slice(1) : path;
+  return `${base}${cleanPath}`;
+}
