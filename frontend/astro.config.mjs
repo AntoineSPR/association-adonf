51584da9b51811afb0a0d6d8fde0b56ea7b9257b
AntoineSPR <antoine.simper@gmail.com>
2026-01-29 11:52:29 +0100

    Fix local env (remove trailing slash in dev)

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://antoinespr.github.io/association-adonf/",
-  base: "/association-adonf/",
+  site: isProd
+    ? "https://antoinespr.github.io/association-adonf/"
+    : "http://localhost:4321",
+  base: isProd ? "/association-adonf/" : "/",
   output: "static",
-  trailingSlash: "always",
+  trailingSlash: isProd ? "always" : "ignore",
 });
