3ffbb12fa8c4518873491342a0b84edcbcad88d5
AntoineSPR <antoine.simper@gmail.com>
2026-04-04 00:04:03 +0200

    Fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,8 @@ const isProd = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: isProd
-    ? "https://antoinespr.github.io/association-adonf/"
-    : "http://localhost:4321",
-  base: isProd ? "/association-adonf/" : "/",
+  site: isProd ? "https://associationadonf.fr" : "http://localhost:4321",
+  base: "/",
   output: "server",
   adapter: node({
     mode: "standalone",
